03c1f96a675e107c846214055c7431f45dfb2566
phuongphamduy <92977825+phuongphamduy@users.noreply.github.com>
2023-11-17 10:56:53 +0700

    order detail

--- src/main/java/com/fahasa/service/impl/OrderServiceImpl.java
+++ src/main/java/com/fahasa/service/impl/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fahasa.dao.OrderDAO;
+import com.fahasa.dao.OrderDetailDAO;
 import com.fahasa.model.Order;
 import com.fahasa.model.OrderDetail;
 import com.fahasa.service.OrderService;
@@ -16,6 +17,9 @@ public class OrderServiceImpl implements OrderService {
 	
 	@Autowired
 	OrderDAO odao;
+	
+	@Autowired
+	OrderDetailDAO ddao;
 
 	@Override
 	public Order create(JsonNode order) {
@@ -24,10 +28,35 @@ public class OrderServiceImpl implements OrderService {
 		Order orderFromDB = odao.getOrderInCartByUser(o.getUser().getId());
 		if(orderFromDB.getId() > 0) {
 			List<OrderDetail> list = o.getOrderdetails();
-			for(int i = 0; i < list.size();i++) {
-				list.get(i).setOrder(orderFromDB);
+			List<OrderDetail> orderDetailFromDB = ddao.getOrderDetailByOrderId(orderFromDB.getId());
+			// lưu orderdetail
+			for(OrderDetail od1 : list) {
+				boolean isSave = false;
+				for(OrderDetail od2 : orderDetailFromDB) {
+					if(od1.getBook() != null && od2.getBook() != null) {
+						if(od1.getBook().getId() == od2.getBook().getId()) {
+							od2.setQuantity(od1.getQuantity() + od2.getQuantity());
+							ddao.save(od2);
+							isSave = true;
+							break;
+						}
+					}
+					if(od1.getSchooltool() != null && od2.getSchooltool() != null) {
+						if(od1.getSchooltool().getId() == od2.getSchooltool().getId()) {
+							od2.setQuantity(od1.getQuantity() + od2.getQuantity());
+							ddao.save(od2);
+							isSave = true;
+							break;
+						}
+					}
+					
+				}
+				if(!isSave) {
+					od1.setOrder(orderFromDB);
+					ddao.save(od1);
+				}
 			}
-			orderFromDB.setOrderdetails(list);
+			
 			orderFromDB.setTotalamount(o.getTotalamount() + orderFromDB.getTotalamount());
 			return odao.save(orderFromDB);
 		}
