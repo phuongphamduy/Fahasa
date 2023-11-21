27a757be59770ff9cefa25f280d0586ec9b9e7d6
Le Duc Tuan <tuanleduc999@gmail.com>
2023-11-21 22:33:03 +0700

    favoriteee

--- src/main/java/com/fahasa/RestController/UserController.java
+++ src/main/java/com/fahasa/RestController/UserController.java
@@ -1,9 +1,15 @@
 package com.fahasa.RestController;
 
+import com.fahasa.dao.UserDAO;
+import com.fahasa.model.Favorite;
+import com.fahasa.model.Order;
 import com.fahasa.model.User;
 import com.fahasa.reponsitory.UserRepository;
 import com.fahasa.service.UserService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +27,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // @GetMapping("/favorite/{id}")
+    // public List<Favorite> getOrderSuccess(@PathVariable("id") Integer id) {
+    //     return uDao.findFavoritesByUserId(id);
+    // }
+
     @GetMapping
     public ResponseEntity<User> getUser() {
         // Lấy thông tin người dùng hiện tại từ Spring Security
@@ -29,6 +40,7 @@ public class UserController {
         // Trả về thông tin người dùng
         return ResponseEntity.ok(user);
     }
+    
 
 
 }
